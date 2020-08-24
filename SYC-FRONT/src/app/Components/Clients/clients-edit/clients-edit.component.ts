89472b75e2353609e8edfdcf9e81411c11136695
jalejandrosandoval5 <61461851+jalejandrosandoval5@users.noreply.github.com>
2020-08-24 15:43:37 -0500

    ClientsEdit -> Ready!

--- SYC-FRONT/src/app/Components/Clients/clients-edit/clients-edit.component.ts
+++ SYC-FRONT/src/app/Components/Clients/clients-edit/clients-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientsModel } from 'src/app/Models/Clients/clients-model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ClientsService } from 'src/app/Services/Clients/clients.service';
 
 @Component({
   selector: 'app-clients-edit',
@@ -7,9 +12,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsEditComponent implements OnInit {
 
-  constructor() { }
+  _SelectedClients: ClientsModel;
+
+  constructor(private formBuilder: FormBuilder, 
+    private toastr: ToastrService,
+    private clientService: ClientsService,
+    private route: ActivatedRoute,
+    private router: Router) {
+
+    }
 
   ngOnInit(): void {
+
+    const UTypeId = this.route.snapshot.params['id_Client'];
+
+    this.setForm(UTypeId);
+
+  }
+
+  ClientsEditForm =this.formBuilder.group({
+    id_Client: [''],
+    numeroDoc: [''],
+    nombre: [''],
+    direccion: [''],
+  });
+
+  private setForm(ClientId: Number){
+
+    this.clientService.getClientsById(ClientId).subscribe(
+      res => 
+      {
+        this._SelectedClients = res,
+        this.ClientsEditForm = this.formBuilder.group({
+          id_Client: [this._SelectedClients.id_Client],
+          numeroDoc: [this._SelectedClients.numeroDoc,{
+            Validators: [Validators.required]
+          }],
+          nombre: [this._SelectedClients.nombre, {
+            Validators: [Validators.required]
+          }],
+          direccion: [this._SelectedClients.direccion, {
+            Validators: [Validators.required]
+          }]
+        })
+      }
+    );
+      
+  }
+
+  get ClientsEditFormData() { 
+    return this.ClientsEditForm.controls; 
+  }
+  
+  onEdit(){
+
+    if (this.ClientsEditForm.invalid) {
+
+      return;
+    
+    }else{
+      
+      this.clientService.putClients(this.ClientsEditForm.value).subscribe(
+        res => {
+          this.toastr.info("¡Actualización Exitosa!", "Clientes:"),
+          this.router.navigateByUrl("clients")
+        },
+        error => this.getError(error), 
+      );
+
+    }
+
+  }
+
+  getError(_Error){
+
+    if (_Error && _Error.error) {
+
+      this.ClientsEditForm.reset;
+      
+      return this.toastr.warning(_Error.error[""], "Error al Actualizar Tipo de Usuarios!");
+      
+    }
+   
   }
 
 }
--- SYC-FRONT/src/app/Components/Invoices/invoices-create/invoices-create.component.ts
+++ SYC-FRONT/src/app/Components/Invoices/invoices-create/invoices-create.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoicesService } from 'src/app/Services/Invoices/invoices.service';
+import { FormBuilder } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { InvoicesStatusService } from 'src/app/Services/Invoices-Status/invoices-status.service';
+import { InvoicesStatusModel } from 'src/app/Models/InvoicesStatus/invoices-status-model';
+import { ClientsModel } from 'src/app/Models/Clients/clients-model';
+import { ClientsService } from 'src/app/Services/Clients/clients.service';
 
 @Component({
   selector: 'app-invoices-create',
@@ -7,9 +15,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoicesCreateComponent implements OnInit {
 
-  constructor() { }
+  _IClient: ClientsModel[];
+  _ClientSelected: ClientsModel;
+  _IInvoicesStatus: InvoicesStatusModel[];
+
+  constructor(private formBuilder: FormBuilder, 
+    private toastr: ToastrService,
+    private clientService: ClientsService,
+    private inoviceService: InvoicesService,
+    private inoviceStatusService: InvoicesStatusService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.getStatuses();
+    this.getClients();
   }
 
+  InvoiceCreateForm = this.formBuilder.group({
+    id_Client: [''],
+    id_Factura: [''],
+    numeroDoc: [''],
+    fechaFac: [''],
+    valor: [''],
+    status: ['']
+  });
+
+  getStatuses(){
+    this.inoviceStatusService.getInvoicesStatus()
+      .subscribe(
+        InvoiceStatus_AWS => this._IInvoicesStatus = InvoiceStatus_AWS,
+        error => this.getError(error)
+    );
+
+  }
+
+  getClients(){
+    this.clientService.getClients().subscribe(
+      Client_AWS => this._IClient = Client_AWS,
+      error => this.getError(error)
+    )
+  }
+
+  getClientSelected(client: ClientsModel){
+    var idClient = client.id_Client;
+
+    this.clientService.getClientsById(idClient).subscribe(
+      res =>
+      {
+        this._ClientSelected = res,
+        this.InvoiceCreateForm =  this.formBuilder.group({
+          id_Client: [this._ClientSelected.id_Client],
+          numeroDoc: [this._ClientSelected.numeroDoc]
+        })
+      }
+    );
+  }
+  
+  onCreate(){
+
+  }
+
+  
+  getError(_Error){
+
+    if (_Error && _Error.error) {
+
+      this.InvoiceCreateForm.reset;
+      
+      return this.toastr.warning(_Error.error[""], "Error al Actualizar Tipo de Usuarios!");
+      
+    }
+   
+  }
 }
